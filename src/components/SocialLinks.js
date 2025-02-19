8de99f56bbdfb44cdea364bc6dc83f6ecc2275bf
Shubhangi <shubhangi.bhosale09@gmail.com>
2025-02-19 11:17:42 +0000

    Updating skill and certification

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -12,8 +12,8 @@ const SocialLinks = () => {
         {
             socialLinks.map(({id, href, download, child, style}) => (
                
-                <li key={id} className={"flex justify-between items-center w-40 h-14 px-3 font-semibold text-xl bg-gray-300 ml-[-110px] hover:ml-[-10px] duration-300 hover:rounded-md" + " "+style}>
-                    <a href={href} className='w-full flex justify-between items-center ml-0' target="_blank" download={download} rel="noreferrer">
+                <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 font-semibold text-xl bg-gray-300 ml-[-110px] hover:ml-[-10px] duration-300 hover:rounded-md" + " "+style}>
+                    <a href={href} className='w-full flex justify-between items-center ml-2' target="_blank" download={download} rel="noreferrer">
                        {child}
                     </a>
                     
