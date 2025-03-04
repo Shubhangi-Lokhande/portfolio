e7e9dcac56498a1cb6a4838aa3cd5e47c435195b
Shubhangi <shubhangi.bhosale09@gmail.com>
2025-03-04 15:22:18 +0000

    Home heading update

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row'>
             <div className='flex flex-col justify-center mb-10 px-10'>
            
-                 <h2 className='text-3xl sm:text-2xl font-semibold text-white w-80'>I'M a Front-End Developer<br/>Exploring Salesforce Administrator Opportunities</h2>
+                 <h2 className='text-3xl sm:text-2xl font-semibold text-white w-80'>I'M a Front-End Developer<br/>with a Salesforce Admin certification, exploring opportunities in both fields.</h2>
                  <p className='font-Courgette sm:text-2xl lg:text-sm text-white w-80 mt-6'>
                     Passionate about building beautiful, user-friendly web applications and diving into the world of Salesforce administration.
                 </p>
@@ -21,7 +21,7 @@ const Home = () => {
                     </Link>
                 </div>
             </div>
-            <div className='pb-2 mt-8'>     
+            <div className='pb-4 mt-8'>     
              <img src={heroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full'/>{/**/}
             </div>
         </div>
